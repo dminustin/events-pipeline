6c5a101fbc2e628a55b0dcdd33b378c70e619133
d.minustin <danila.minustin@gmail.com>
2022-04-04 21:26:19 +0300

    config

--- config/events-pipeline.php
+++ config/events-pipeline.php
@@ -2,8 +2,9 @@
 
 return [
     'watcher_sleep_ms'=> '10000',
-    'manager' => '',
+    'manager' => \App\Classes\EventsPipeline\QueueManagers\FilesQueueManager::class, //
     'mappging' => [
-        
+        'event1' => ['pipe1', 'pipe2', 'pipe3'], //Multiple pipes per event
+        'event2' => ['pipe3'], //Multiple pipes per event
     ]
 ];
